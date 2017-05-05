d9ddd1410b98ce2776a6d18460ee8b97c640b1b3
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-05 11:38:49 -0700

    correctly invokes the search function

--- public/components/search.js
+++ public/components/search.js
@@ -1,1 +1,15 @@
-search.js
+angular.module('app')
+  .directive('search', function() {
+    return {
+      scope: {
+        searchLocation: '<'
+      },
+      restrict: 'E',
+      controllerAs: 'ctrl',
+      bindToController: true,
+      controller: function($scope) {
+        console.log($scope)
+      },
+      templateUrl: '../templates/search.html'
+    }
+  })
