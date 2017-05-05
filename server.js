22e3b436f2e957861cade9d8c39d0c7c26fb9c03
TylerCorkill <TylerCorkill@gmail.com>
2017-05-05 15:25:01 -0700

    auto create db with script when server starts

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var server = require('./routes.js');
+var exec = require('child_process').exec;
+
+exec('mysql -u root < db/script.sql');
 
 server.listen(8000, function() {
   console.log('Server is listening on 8000');
