2ba184cf7a4671236d6c92f23b1afade9567d5aa
TylerCorkill <TylerCorkill@gmail.com>
2017-05-04 23:51:49 -0700

    serving up geocoding results, closes #28

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path');
+    request = require('request');
 
 
 // var viewPath = path.join(__dirname+'/public/views/');
@@ -14,7 +15,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/submit_location', (req, res) => {
-  res.end('location placeholder');
+  var propertiesObj = { address: req.body.text,
+    key: 'AIzaSyBZ8EbK7eX0twoYIy-wfONHc29fZJU3HV8' };
+
+  var params = {url: 'https://maps.googleapis.com/maps/api/geocode/json', qs: propertiesObj};
+
+  request(params, function(err, response, body) {
+    if(err) { console.log(err); return; }
+    body = JSON.parse(body);
+    res.end( JSON.stringify(body.results) );
+  });
 });
 
 
