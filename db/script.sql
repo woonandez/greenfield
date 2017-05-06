f78a62ef9f67470c2794e94509478e7448780b80
Parul bir <parul.bir@gmail.com>
2017-05-05 17:45:38 -0700

    done setting up models in sequelize

--- db/script.sql
+++ db/script.sql
@@ -10,8 +10,10 @@ CREATE TABLE `itineraries` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
   `start` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `end` DATETIME NOT NULL,
+  `end` DATETIME,
   `userId` INTEGER NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -20,12 +22,14 @@ DROP TABLE IF EXISTS `locations`;
 CREATE TABLE `locations` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `location` VARCHAR(200) NOT NULL ,
-  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `visitDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `longitude` DOUBLE NOT NULL,
   `latitude` DOUBLE NOT NULL,
-  `itineraryId` INTEGER NOT NULL,
+  `id_itineraries` INTEGER NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `locations` ADD FOREIGN KEY (itineraryId) REFERENCES `itineraries` (`id`);
+ALTER TABLE `locations` ADD FOREIGN KEY (id_itineraries) REFERENCES `itineraries` (`id`);
 
