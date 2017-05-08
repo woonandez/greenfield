88277599688ee412e6b4699af459b84517760619
Home <home@Homes-MacBook-Air.local>
2017-05-08 11:11:10 -0700

    some code cleanup

--- public/index.js
+++ public/index.js
@@ -3,41 +3,25 @@ angular.module('app')
     $scope.mapCenter = 'San Francisco';
     $scope.mapType = 'TERRAIN';
 
-    $scope.changeMapType = function() {
-      $scope.mapType === 'TERRAIN' ? $scope.mapType = 'SATELLITE' : $scope.mapType = 'TERRAIN';
-    };
-
     $scope.getCurrentLocation = function(e) {
       var lat = e.latLng.lat();
       var long = e.latLng.lng();
       $scope.mapCenter = [lat, long];
-      console.log('lat', lat, 'long', long);
     }
-
-//     $scope.getCurrentLocation = function(event){
-//         console.log(event.latLng.lat());
-//         console.log(event.latLng.lng());
-// }
 
     $scope.goAnchor = function (event) {
       console.log(this.id);
       gotoAnchor(this.id);
-    };
+    }
 
     NgMap.getMap().then(function(map) {
       map.getCenter();
       console.log(map);
-      // console.log('markers', map.markers);
-      // var user = {
-      //   cityLoc: $scope.mapCenter,
-      //   date: '',
-      //   time: '',
-      //   uid: ''
-      // }
+      // this function will be used to initialize all of the different markers on the map
+        // the markers that correspond to the items in the itinerary
     });
 
     $scope.searchLocation = function(newLoc) {
       $scope.mapCenter = newLoc;
     }
-
   })
