dac52106cf515513299d1250f55da3d6899f4cf7
TylerCorkill <TylerCorkill@gmail.com>
2017-05-08 11:38:06 -0700

    added dummy response for /locations_for_itinerary

--- db/script.sql
+++ db/script.sql
@@ -8,15 +8,9 @@ USE piranha;
 
 CREATE TABLE IF NOT EXISTS `itineraries` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-<<<<<<< HEAD
-  `name` VARCHAR(50) NOT NULL,
-  `start` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `end` DATETIME,
-=======
   `name` TEXT NOT NULL,
   `start` TEXT NOT NULL,
   `end` TEXT NOT NULL,
->>>>>>> 6ab9b2216560aa284613746ce00026ead96c9150
   `userId` INTEGER NOT NULL,
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NOT NULL,
@@ -27,7 +21,6 @@ CREATE TABLE IF NOT EXISTS `itineraries` (
 
 CREATE TABLE  IF NOT EXISTS `locations` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-<<<<<<< HEAD
   `location` VARCHAR(200) NOT NULL ,
   `visitDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `longitude` DOUBLE NOT NULL,
@@ -35,15 +28,6 @@ CREATE TABLE  IF NOT EXISTS `locations` (
   `id_itineraries` INTEGER NOT NULL,
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NOT NULL,
-=======
-  `itineraryId` INTEGER NOT NULL,
-  `location` TEXT NOT NULL ,
-  `date` TEXT NOT NULL,
-  `time` TEXT NOT NULL,
-  `longitude` INTEGER NOT NULL,
-  `latitude` INTEGER NOT NULL,
-
->>>>>>> 6ab9b2216560aa284613746ce00026ead96c9150
   PRIMARY KEY (`id`)
 );
 
