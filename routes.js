315a6075db816925bc8e01bd091a82734dcebca7
TylerCorkill <TylerCorkill@gmail.com>
2017-05-09 14:20:53 -0700

    refactored routes

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path');
     request = require('request'),
-    helpers = require('./db/helper');
+    db = require('./db/helper');
 
 
 var exec = require('child_process').exec;
@@ -27,7 +27,6 @@ app.get('/', (req, res) => {
 
 // form should send request with text from an input element named 'text'
 app.post('/submit_location', (req, res) => {
-
   // req.body === {
   //   itineraryId: 0,
   //   text: "google hq",
@@ -52,6 +51,8 @@ app.post('/submit_location', (req, res) => {
     } else {
       var results = JSON.parse( body ).results[0];
 
+
+      // function(itineraryId, location, visitDate, time, longitude, latitude, callback)
       var args = [
         req.body.itineraryId,
         results.formatted_address,
@@ -60,6 +61,8 @@ app.post('/submit_location', (req, res) => {
         results.geometry.location.lat,
         results.geometry.location.lng,
       ];
+
+      console.log(args);
 
       var responseObj = {
         location: results.formatted_address,
@@ -70,45 +73,103 @@ app.post('/submit_location', (req, res) => {
         placeID: results.place_id
       };
 
-      // helpers.addLocation(...args, function() {
-        res.end( JSON.stringify(responseObj) );
-      // });
+      db.addLocation(...args, function() {});
 
+      res.end( JSON.stringify(responseObj) );
 
     }
   });
 });
 
 
-app.get('/locations_for_itinerary', (req, res) => {
-  console.log(req.query.itineraryId);
-  res.end( JSON.stringify( [ {
-    location: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
-    visitDate: "July 4th",
-    id: 1,
-    latitude: 37.421999,
-    longitude: -122.0840575
-  },
-  {
-    location: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
-    visitDate: "July 4th",
-    id: 2,
-    latitude: 33.421999,
-    longitude: -122.0840575
-  },
-  {
-    location: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
-    visitDate: "July 4th",
-    id: 3,
-    latitude: 51.421999,
-    longitude: -122.0840575
-  } ]));
+
+
+
+app.post('/submit_itinerary', (req, res) => {
+  // req.body === {
+  //   name: 'trip name',
+  //   start: 'start date',
+  //   end: 'end date',
+  //   userID: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3hvc2suYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4MzU4MTMyNzk4ODgxNzc2ODg4IiwiYXVkIjoieDdJdGk3MUpKVjZhcHBZN3BwT0w2WGFqaTFoSDRGbUIiLCJleHAiOjE0OTQzODM3OTMsImlhdCI6MTQ5NDM0Nzc5M30.piHQCL1aHMlzgTZGzdkzm1s3lOvmlisn036MZkOp0Xc'
+  // }
+
+
+
+
+  // input(name, start, end, userId, callback)
+  db.addItinerary(req.body.name, req.body.start, req.body.end, req.body.userId, function() {
+    res.end('itinerary created');
+  });
+
+
+
 });
 
-app.post('/login', (req, res) => {
-  console.log('/login', req.body);
-  // res.end();
+
+
+
+
+
+app.get('/locations_for_itinerary', (req, res) => {
+  // req.query === {
+  //   itineraryId: 0
+  // }
+
+  console.log(req.query.itineraryId);
+
+  db.getitineraryLocations(req.query.itineraryId, (result) => {
+    res.end( JSON.stringify(result) );
+  });
+
+
+  // ------------- dummy data -------------
+  // res.end( JSON.stringify( [ {
+  //   location: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+  //   visitDate: "July 4th",
+  //   id: 1,
+  //   latitude: 37.421999,
+  //   longitude: -122.0840575
+  // },
+  // {
+  //   location: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+  //   visitDate: "July 4th",
+  //   id: 2,
+  //   latitude: 33.421999,
+  //   longitude: -122.0840575
+  // },
+  // {
+  //   location: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+  //   visitDate: "July 4th",
+  //   id: 3,
+  //   latitude: 51.421999,
+  //   longitude: -122.0840575
+  // } ]));
+  // ---------------------------------------
+
+
+
 });
+
+
+
+
+app.post('/login', (req, res) => {
+  // req.body === {
+  //   user_id: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3hvc2suYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4MzU4MTMyNzk4ODgxNzc2ODg4IiwiYXVkIjoieDdJdGk3MUpKVjZhcHBZN3BwT0w2WGFqaTFoSDRGbUIiLCJleHAiOjE0OTQzODM3OTMsImlhdCI6MTQ5NDM0Nzc5M30.piHQCL1aHMlzgTZGzdkzm1s3lOvmlisn036MZkOp0Xc"
+  // }
+
+  console.log('/login', req.body);
+  db.getUserItineraries(req.body.user_id, (res) => {
+    if (!res.length) {
+      db.addItinerary(req.body.name, req.body.start, req.body.end, req.body.userId, function() {
+        res.end('itinerary created for new user');
+      });
+    }
+  });
+});
+
+
+
 
 
 app.post('/signup', (req, res) => {
