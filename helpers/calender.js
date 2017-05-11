3b95c6a333630ac67d9351d73c1e090ade9adfe7
Parul bir <parul.bir@gmail.com>
2017-05-11 14:02:45 -0700

    skelton for google calender api

---
+++ helpers/calender.js
@@ -0,0 +1,7 @@
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var createCalender = function(accessToken, refreshToken, tokenType, expiryDate) {
+
+}
+// oauth credentials {"access_token":"ya29.GltGBLvPWikRzfkrIXltZ1NY9rxZvzypfK0_cl_utqnNn6lsdAmYk6OjqnyLWA2MCHC1b4jCIG5jKNCTBwpk5L7-Nf-V3N-XoODpVGQhfi_2xNpyDvz-rRkcxrgW","refresh_token":"1/6aYL8M4RMJOG1gwWxPJpxe9M19yGB_jd5Mlvq7EeWNKvsRAiJu_qUIVHwOhwL_pV","token_type":"Bearer","expiry_date":1494462791046}
